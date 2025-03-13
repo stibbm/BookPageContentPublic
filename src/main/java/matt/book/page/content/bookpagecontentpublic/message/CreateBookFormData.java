3cfa42971479d4a3f23cbb3b946661613f5b4e7e
matthewstibbins <matthewstibbins@gmail.com>
2025-03-13 00:31:29 -0700

    add CreateBookFormData message

---
+++ src/main/java/matt/book/page/content/bookpagecontentpublic/message/CreateBookFormData.java
@@ -0,0 +1,23 @@
+package matt.book.page.content.bookpagecontentpublic.message;
+
+import java.io.Serializable;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.auto.value.AutoValue.Builder;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateBookFormData implements Serializable {
+	private String bookName;
+	private String bookDescription;
+	private String bookLanguage;
+	private String bookTags;
+	private MultipartFile bookThumbnailFile;
+}
