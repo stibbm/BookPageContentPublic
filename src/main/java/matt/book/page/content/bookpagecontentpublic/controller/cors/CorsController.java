26cfd85f30671c2fb85c65232af6cfa42a51f109
matthewstibbins <matthewstibbins@gmail.com>
2025-03-14 12:48:33 -0700

    implement CorsController

--- src/main/java/matt/book/page/content/bookpagecontentpublic/controller/cors/CorsController.java
+++ src/main/java/matt/book/page/content/bookpagecontentpublic/controller/cors/CorsController.java
@@ -1,4 +1,4 @@
-package matt.book.page.content.bookpagecontentpublic.cors;
+package matt.book.page.content.bookpagecontentpublic.controller.cors;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
